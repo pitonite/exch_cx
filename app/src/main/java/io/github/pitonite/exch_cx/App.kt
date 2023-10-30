fa5b559ca59e57201060c0ec0b156b03b45c45cf
pitonite <pitonite@proton.me>
2023-10-30 06:00:48 +0330

    chore: add support for work manager for hilt

--- app/src/main/java/io/github/pitonite/exch_cx/App.kt
+++ app/src/main/java/io/github/pitonite/exch_cx/App.kt
@@ -1,6 +1,15 @@
 package io.github.pitonite.exch_cx
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
-@HiltAndroidApp class App : Application() {}
+@HiltAndroidApp
+class App : Application(), Configuration.Provider {
+  @Inject lateinit var workerFactory: HiltWorkerFactory
+
+  override fun getWorkManagerConfiguration() =
+      Configuration.Builder().setWorkerFactory(workerFactory).build()
+}
