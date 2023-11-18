8176d8e9f75dcb279e855f0c58c586c1bdf43b74
pitonite <pitonite@proton.me>
2023-11-18 23:54:21 +0330

    fix: some aesthetics with confirming input state

--- app/src/main/java/io/github/pitonite/exch_cx/ui/screens/orderdetail/components/states/OrderConfirmingInput.kt
+++ app/src/main/java/io/github/pitonite/exch_cx/ui/screens/orderdetail/components/states/OrderConfirmingInput.kt
@@ -1,15 +1,28 @@
 package io.github.pitonite.exch_cx.ui.screens.orderdetail.components.states
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.InlineTextContent
+import androidx.compose.foundation.text.appendInlineContent
 import androidx.compose.foundation.text.selection.SelectionContainer
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.Placeholder
+import androidx.compose.ui.text.PlaceholderVerticalAlign
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import io.github.pitonite.exch_cx.R
 import io.github.pitonite.exch_cx.data.OrderRepositoryMock.Companion.orderConfirmingInput
 import io.github.pitonite.exch_cx.data.OrderRepositoryMock.Companion.orderConfirmingInputEthNote
@@ -26,16 +39,42 @@ fun OrderConfirmingInput(
     Text(
         stringResource(R.string.order_state_confirming_input_title),
         style = MaterialTheme.typography.headlineSmall,
-    )
+        modifier = Modifier.padding(end = dimensionResource(R.dimen.padding_sm)))
 
-    Text(stringResource(R.string.order_state_confirming_input_desc))
+
+    val desc = stringResource(R.string.order_state_confirming_input_desc)
+    val annotatedDesc = buildAnnotatedString {
+      append(desc)
+      append("  ")
+      pushStringAnnotation(tag = "indicator", annotation = "indicator")
+      appendInlineContent(
+          "indicator", "([${stringResource(R.string.accessibility_label_working)}])")
+      pop()
+    }
+    val indicatorSize = LocalTextStyle.current.fontSize
+    Text(annotatedDesc,
+        inlineContent =
+        mapOf(
+            Pair(
+                "indicator",
+                InlineTextContent(
+                    Placeholder(
+                        width = indicatorSize,
+                        height = indicatorSize,
+                        placeholderVerticalAlign = PlaceholderVerticalAlign.Center)
+                ) {
+                  CircularProgressIndicator()
+                },
+            )))
 
     Spacer(Modifier)
 
     if (order.transactionIdReceived != null) {
       Column {
         Text(stringResource(R.string.label_detected_transaction_id))
-        SelectionContainer { TransactionText(currency = order.fromCurrency, txid = order.transactionIdReceived) }
+        SelectionContainer {
+          TransactionText(currency = order.fromCurrency, txid = order.transactionIdReceived)
+        }
       }
     } else if (order.fromCurrency.lowercase() == "eth") {
       Text(stringResource(R.string.notice_eth_txid_notice))
@@ -46,7 +85,6 @@ fun OrderConfirmingInput(
         Text(stringResource(R.string.detected_amount, order.fromAmountReceived, order.fromCurrency))
       }
     }
-
   }
 }
 
