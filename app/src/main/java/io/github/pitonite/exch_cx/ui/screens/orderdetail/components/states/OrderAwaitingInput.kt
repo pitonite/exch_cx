3a86024847dce733be6a58c34320c293f2ba3a77
pitonite <pitonite@proton.me>
2023-11-18 14:05:25 +0330

    chore: cleanup OrderAwaitingInput a bit

--- app/src/main/java/io/github/pitonite/exch_cx/ui/screens/orderdetail/components/states/OrderAwaitingInput.kt
+++ app/src/main/java/io/github/pitonite/exch_cx/ui/screens/orderdetail/components/states/OrderAwaitingInput.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material3.CircularProgressIndicator
@@ -29,9 +28,9 @@ import io.github.pitonite.exch_cx.data.OrderRepositoryMock.Companion.orderAwaiti
 import io.github.pitonite.exch_cx.data.OrderRepositoryMock.Companion.orderAwaitingInputWithFromAmount
 import io.github.pitonite.exch_cx.data.room.GENERATING_FROM_ADDRESS
 import io.github.pitonite.exch_cx.data.room.Order
-import io.github.pitonite.exch_cx.ui.components.Card
 import io.github.pitonite.exch_cx.ui.components.CopyableText
 import io.github.pitonite.exch_cx.ui.components.Notice
+import io.github.pitonite.exch_cx.ui.screens.orderdetail.components.OrderStateCard
 import io.github.pitonite.exch_cx.ui.screens.orderdetail.etheriumBasedCoins
 import io.github.pitonite.exch_cx.ui.theme.ExchTheme
 import io.github.pitonite.exch_cx.utils.rememberQrBitmapPainter
@@ -39,130 +38,109 @@ import java.math.BigDecimal
 
 @Composable
 fun OrderAwaitingInput(
-  order: Order,
+    order: Order,
 ) {
-  if (order.maxInput.compareTo(BigDecimal.ZERO) == 0) {
-    Column(
-        modifier =
-        Modifier
-            .padding(horizontal = dimensionResource(R.dimen.padding_lg))
-            .padding(
-                vertical = dimensionResource(R.dimen.padding_lg),
-            ),
-        verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_lg)),
-    ) {
+  OrderStateCard {
+    if (order.maxInput.compareTo(BigDecimal.ZERO) == 0) {
+
       Text(stringResource(R.string.order_state_max_input_zero, order.fromCurrency))
-    }
-  } else {
-    Card {
-      Column(
-          modifier =
-          Modifier
-              .padding(horizontal = dimensionResource(R.dimen.padding_lg))
-              .padding(
-                  vertical = dimensionResource(R.dimen.padding_lg),
-              ),
-          verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_lg)),
-      ) {
-        Text(
-            stringResource(R.string.order_state_title_created, order.fromCurrency),
-            style = MaterialTheme.typography.headlineSmall,
-        )
+    } else {
+      Text(
+          stringResource(R.string.order_state_title_waiting_for_input, order.fromCurrency),
+          style = MaterialTheme.typography.headlineSmall,
+      )
 
-        Spacer(Modifier)
+      Spacer(Modifier)
 
-        if (order.fromAmount != null) {
-          SelectionContainer {
-            Text(stringResource(R.string.label_send, order.fromAmount, order.fromCurrency))
-          }
-          SelectionContainer {
-            val hundred = BigDecimal.valueOf(100)
-            val receiveAmount =
-                order.fromAmount
-                    .times(
-                        BigDecimal.valueOf(100)
-                            .minus(order.svcFee)
-                            .divide(hundred)
-                            .times(order.rate),
-                    )
-                    .minus(order.networkFee ?: BigDecimal.ZERO)
-                    .stripTrailingZeros()
-            Text(
-                stringResource(R.string.label_receive, receiveAmount, order.toCurrency) +
-                    " (${stringResource(R.string.network_fee_included)})",
-            )
-          }
-        } else {
-          SelectionContainer {
-            Text(stringResource(R.string.label_minimum, order.minInput, order.fromCurrency))
-          }
-
-          SelectionContainer {
-            Text(stringResource(R.string.label_maximum, order.maxInput, order.fromCurrency))
-          }
+      if (order.fromAmount != null) {
+        SelectionContainer {
+          Text(stringResource(R.string.label_send, order.fromAmount, order.fromCurrency))
+        }
+        SelectionContainer {
+          val hundred = BigDecimal.valueOf(100)
+          val receiveAmount =
+              order.fromAmount
+                  .times(
+                      BigDecimal.valueOf(100).minus(order.svcFee).divide(hundred).times(order.rate),
+                  )
+                  .minus(order.networkFee ?: BigDecimal.ZERO)
+                  .stripTrailingZeros()
+          Text(
+              stringResource(R.string.label_receive, receiveAmount, order.toCurrency) +
+                  " (${stringResource(R.string.network_fee_included)})",
+          )
+        }
+      } else {
+        SelectionContainer {
+          Text(stringResource(R.string.label_minimum, order.minInput, order.fromCurrency))
         }
 
-        Spacer(Modifier)
+        SelectionContainer {
+          Text(stringResource(R.string.label_maximum, order.maxInput, order.fromCurrency))
+        }
+      }
 
-        Column(verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_md))) {
-          Text(stringResource(R.string.label_to_order_address, order.fromCurrency))
+      Spacer(Modifier)
 
-          if (order.fromAddr != GENERATING_FROM_ADDRESS) {
-            CopyableText(
-                order.fromAddr,
-                copyConfirmationMessage = R.string.snack_address_copied,
-                fontSize = 19.sp,
-            )
+      Column(verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_md))) {
+        Text(stringResource(R.string.label_to_order_address, order.fromCurrency))
 
-            if (order.stateError == null &&
-              etheriumBasedCoins.containsMatchIn(order.fromCurrency)) {
-              Notice(stringResource(R.string.notice_etherium_based_coins))
-            }
+        if (order.fromAddr != GENERATING_FROM_ADDRESS) {
+          CopyableText(
+              order.fromAddr,
+              copyConfirmationMessage = R.string.snack_address_copied,
+              fontSize = 19.sp,
+          )
 
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.Center,
-            ) {
-              Image(
-                  painter =
-                  rememberQrBitmapPainter(
-                      content = order.fromAddr, size = 300.dp, padding = 1.dp,
-                  ),
-                  contentDescription =
-                  stringResource(R.string.desc_send_qrcode_image, order.fromCurrency),
-                  modifier = Modifier.clip(MaterialTheme.shapes.large),
-              )
-            }
+          if (order.stateError == null && etheriumBasedCoins.containsMatchIn(order.fromCurrency)) {
+            Notice(stringResource(R.string.notice_etherium_based_coins))
+          }
 
-            if (order.fromAmount != null) {
-              Notice {
-                Text(
-                    stringResource(
-                        R.string.notice_order_different_amount,
-                        order.fromAmount,
-                        order.fromCurrency,
+          Row(
+              modifier = Modifier.fillMaxWidth(),
+              horizontalArrangement = Arrangement.Center,
+          ) {
+            Image(
+                painter =
+                    rememberQrBitmapPainter(
+                        content = order.fromAddr,
+                        size = 300.dp,
+                        padding = 1.dp,
                     ),
-                    textAlign = TextAlign.Justify,
-                )
+                contentDescription =
+                    stringResource(R.string.desc_send_qrcode_image, order.fromCurrency),
+                modifier = Modifier.clip(MaterialTheme.shapes.large),
+            )
+          }
 
-                SelectionContainer {
-                  Text(stringResource(R.string.label_minimum, order.minInput, order.fromCurrency))
-                }
+          if (order.fromAmount != null) {
+            Notice {
+              Text(
+                  stringResource(
+                      R.string.notice_order_different_amount,
+                      order.fromAmount,
+                      order.fromCurrency,
+                  ),
+                  textAlign = TextAlign.Justify,
+              )
 
-                SelectionContainer {
-                  Text(stringResource(R.string.label_maximum, order.maxInput, order.fromCurrency))
-                }
+              SelectionContainer {
+                Text(stringResource(R.string.label_minimum, order.minInput, order.fromCurrency))
+              }
+
+              SelectionContainer {
+                Text(stringResource(R.string.label_maximum, order.maxInput, order.fromCurrency))
               }
             }
-          } else {
-            Row(verticalAlignment = Alignment.CenterVertically) {
-              Text(
-                  stringResource(R.string.address_generating) + " ",
-                  color = MaterialTheme.colorScheme.onSurfaceVariant,
-                  fontSize = MaterialTheme.typography.bodyLarge.fontSize,
-              )
-              CircularProgressIndicator(Modifier.size(24.dp))
-            }
+          }
+        } else {
+          Row(verticalAlignment = Alignment.CenterVertically) {
+            Text(
+                stringResource(R.string.address_generating) + " ",
+                color = MaterialTheme.colorScheme.onSurfaceVariant,
+                fontSize = MaterialTheme.typography.bodyLarge.fontSize,
+            )
+            CircularProgressIndicator(Modifier.size(24.dp))
           }
         }
       }
