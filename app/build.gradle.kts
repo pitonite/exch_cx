a93e3ec4b33ddf9bba0bbb2355a7f6d4f2864aed
pitonite <pitonite@proton.me>
2023-11-25 02:01:52 +0330

    chore: bump version to 1.0.3 (5)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
     applicationId = "io.github.pitonite.exch_cx"
     minSdk = 24
     targetSdk = 34
-    versionCode = 4
-    versionName = "1.0.2"
+    versionCode = 5
+    versionName = "1.0.3"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     vectorDrawables { useSupportLibrary = true }
