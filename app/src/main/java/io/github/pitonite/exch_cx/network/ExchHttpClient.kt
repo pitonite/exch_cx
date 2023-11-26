f37b82c5a22c4383d9975cca4b9c59c754159c2e
pitonite <pitonite@proton.me>
2023-11-26 04:47:01 +0330

    chore: lint ExchHttpClient.kt

--- app/src/main/java/io/github/pitonite/exch_cx/network/ExchHttpClient.kt
+++ app/src/main/java/io/github/pitonite/exch_cx/network/ExchHttpClient.kt
@@ -12,7 +12,6 @@ import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.engine.ProxyConfig
 import io.ktor.client.engine.okhttp.OkHttp
-import io.ktor.client.plugins.BrowserUserAgent
 import io.ktor.client.plugins.HttpRequestRetry
 import io.ktor.client.plugins.HttpResponseValidator
 import io.ktor.client.plugins.HttpTimeout
@@ -115,9 +114,7 @@ private fun createHttpClient(
       }
     }
 
-    install(UserAgent) {
-      agent = "eXch mobile client/v${BuildConfig.VERSION_NAME}"
-    }
+    install(UserAgent) { agent = "eXch mobile client/v${BuildConfig.VERSION_NAME}" }
 
     defaultRequest {
       if (!headers.contains("X-Requested-With")) {
