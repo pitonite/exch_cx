3bd96c3402867349d001dc49ae57b076c128cfd4
pitonite <pitonite@proton.me>
2023-12-26 16:12:17 +0330

    chore: update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,13 +51,13 @@ android {
 dependencies {
   implementation("androidx.core:core-ktx:1.12.0")
   implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-  implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0-rc01")
-  implementation("androidx.activity:activity-compose:1.8.1")
+  implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0-rc02")
+  implementation("androidx.activity:activity-compose:1.8.2")
   implementation(platform("androidx.compose:compose-bom:2023.10.01"))
   implementation("androidx.compose.ui:ui")
   implementation("androidx.compose.ui:ui-graphics")
   implementation("androidx.compose.ui:ui-tooling-preview")
-  implementation("androidx.compose.material3:material3:1.2.0-alpha09")
+  implementation("androidx.compose.material3:material3:1.2.0-beta01")
   // for adaptive layout
   implementation("androidx.compose.material3:material3-window-size-class")
 
@@ -71,7 +71,7 @@ dependencies {
   testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
 
   // navigation
-  implementation("androidx.navigation:navigation-compose:2.7.5")
+  implementation("androidx.navigation:navigation-compose:2.7.6")
 
   // hilt
   implementation("com.google.dagger:hilt-android:2.48.1")
@@ -81,7 +81,7 @@ dependencies {
   // for work manager
   implementation("androidx.hilt:hilt-work:1.1.0")
   ksp("androidx.hilt:hilt-compiler:1.1.0")
-  implementation("androidx.work:work-runtime-ktx:2.8.1")
+  implementation("androidx.work:work-runtime-ktx:2.9.0")
 
   // for locale change to work, all activities need to extend AppCompatActivity
   val appcompatVersion = "1.7.0-alpha03"
@@ -110,7 +110,7 @@ dependencies {
   implementation("com.google.protobuf:protobuf-kotlin-lite:$protobufVersion")
 
   // for room
-  val roomVersion = "2.6.0"
+  val roomVersion = "2.6.1"
   implementation("androidx.room:room-runtime:$roomVersion")
   annotationProcessor("androidx.room:room-compiler:$roomVersion")
   ksp("androidx.room:room-compiler:$roomVersion")
