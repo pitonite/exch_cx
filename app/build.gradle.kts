2e8db45bc981d42c4d619561eb8ecd706c507e3b
pitonite <pitonite@proton.me>
2023-12-26 16:10:40 +0330

    chore: update changelog and bump versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
     applicationId = "io.github.pitonite.exch_cx"
     minSdk = 24
     targetSdk = 34
-    versionCode = 11
-    versionName = "1.1.1"
+    versionCode = 12
+    versionName = "1.1.2"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     vectorDrawables { useSupportLibrary = true }
