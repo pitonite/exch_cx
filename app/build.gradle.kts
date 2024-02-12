3832f21368cd4e6c83afdc0ef261e694d57166a3
pitonite <pitonite@proton.me>
2024-02-12 13:53:06 +0330

    chore: update android test implementation bom version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,7 +58,10 @@ dependencies {
   implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
   implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0")
   implementation("androidx.activity:activity-compose:1.8.2")
+
   implementation(platform("androidx.compose:compose-bom:2024.02.00"))
+  androidTestImplementation(platform("androidx.compose:compose-bom:2024.02.00"))
+
   implementation("androidx.compose.ui:ui")
   implementation("androidx.compose.ui:ui-graphics")
   implementation("androidx.compose.ui:ui-tooling-preview")
@@ -69,7 +72,6 @@ dependencies {
   testImplementation("junit:junit:4.13.2")
   androidTestImplementation("androidx.test.ext:junit:1.1.5")
   androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-  androidTestImplementation(platform("androidx.compose:compose-bom:2023.10.01"))
   androidTestImplementation("androidx.compose.ui:ui-test-junit4")
   debugImplementation("androidx.compose.ui:ui-tooling")
   debugImplementation("androidx.compose.ui:ui-test-manifest")
